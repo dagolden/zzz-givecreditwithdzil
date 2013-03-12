e7bb749750c50d00288999f1e143ede9e4e69207
David Golden <dagolden@cpan.org>
2013-03-12 14:58:43 -0400

    import newly minted distribution

---
+++ lib/Acme/GiveCreditWithDzil.pm
@@ -0,0 +1,23 @@
+use 5.008001;
+use strict;
+use warnings;
+
+package Acme::GiveCreditWithDzil;
+# ABSTRACT: Demonstrates how to credit contributors
+
+1;
+
+=for Pod::Coverage method_names_here
+
+=head1 SYNOPSIS
+
+  use Acme::GiveCreditWithDzil;
+
+=head1 DESCRIPTION
+
+This module might be cool, but you'd never know it from the lack
+of documentation.
+
+=cut
+
+# vim: ts=4 sts=4 sw=4 et:
